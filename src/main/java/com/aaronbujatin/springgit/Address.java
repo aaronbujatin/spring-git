c869587b1ec3724bc5f01e083709820033f74828
Aaron Bujatin <aaronbujatin@gmail.com>
2023-07-24 18:37:48 +0800

    users and address object created

---
+++ src/main/java/com/aaronbujatin/springgit/Address.java
@@ -0,0 +1,11 @@
+package com.aaronbujatin.springgit;
+
+public class Address {
+
+    private String city;
+    private String street;
+    private String state;
+    private int postalCode;
+    private String country;
+
+}
---
+++ src/main/java/com/aaronbujatin/springgit/Users.java
@@ -0,0 +1,8 @@
+package com.aaronbujatin.springgit;
+
+public class Users {
+
+    private String name;
+    private String email;
+    private Address address;
+}
